f118134c3fb0bf1e16d05d4759ed8f16e9fc4d6d
Marcel Klehr <mklehr@gmx.net>
2022-11-23 15:50:00 +0100

    Make app run

---
+++ lib/AppInfo/Application.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ * Copyright (c) 2021. The Nextcloud Recognize contributors.
+ *
+ * This file is licensed under the Affero General Public License version 3 or later. See the COPYING file.
+ */
+
+namespace OCA\Files_Confidential\AppInfo;
+
+use OCP\AppFramework\App;
+use OCP\AppFramework\Bootstrap\IBootContext;
+use OCP\AppFramework\Bootstrap\IBootstrap;
+use OCP\AppFramework\Bootstrap\IRegistrationContext;
+
+class Application extends App implements IBootstrap {
+	public const APP_ID = 'files_confidential';
+
+	public function __construct() {
+		parent::__construct(self::APP_ID);
+	}
+
+	public function register(IRegistrationContext $context): void {
+		@include_once __DIR__ . '/../../vendor/autoload.php';
+	}
+
+	/**
+	 * @throws \Throwable
+	 */
+	public function boot(IBootContext $context): void {
+	}
+}
