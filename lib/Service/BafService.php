d7de20b77333c784912f50374294fb0e4f999a6c
Marcel Klehr <mklehr@gmx.net>
2022-12-14 13:24:12 +0100

    Implement BafService#parseXml and tests
    Signed-off-by: Marcel Klehr <mklehr@gmx.net>

---
+++ lib/Service/BafService.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace OCA\Files_Confidential\Service;
+
+use OCA\Files_Confidential\Contract\IClassificationLabel;
+use OCA\Files_Confidential\Model\ClassificationLabel;
+use Sabre\Xml\ParseException;
+use Sabre\Xml\Reader;
+use Sabre\Xml\Service;
+
+class BafService {
+	public const ELEMENT_BUSINESS_AUTHORIZATION = '{urn:tscp:names:baf:1.1}BusinessAuthorization';
+	public const ELEMENT_INCLUDED = '{urn:tscp:names:baf:1.1}Included';
+	public const ELEMENT_CATEGORY = '{urn:tscp:names:baf:1.1}BusinessAuthorizationCategory';
+
+	/**
+	 * @param string $xml
+	 * @return IClassificationLabel[]
+	 */
+	public function parseXml(string $xml) : array {
+		$service = new Service();
+		$service->elementMap = [
+			self::ELEMENT_BUSINESS_AUTHORIZATION => function (Reader $reader) {
+				$children = $reader->parseInnerTree();
+				foreach ($children as $child) {
+					if ($child['name'] === self::ELEMENT_INCLUDED) {
+						return $child['value'];
+					}
+				}
+				return [] ;
+			},
+			self::ELEMENT_INCLUDED => function (Reader $reader) {
+				$children = $reader->parseInnerTree();
+				$categories = [];
+				$i = 0;
+				foreach ($children as $child) {
+					if ($child['name'] === self::ELEMENT_CATEGORY) {
+						$categories[] = new ClassificationLabel(
+							$i++,
+							$child['attributes']['Name'],
+							[$child['attributes']['Name']],
+							[$child['attributes']['Identifier']],
+						);
+					}
+				}
+				return $categories;
+			}
+		];
+
+
+		try {
+			return $service->parse($xml);
+		} catch (ParseException $e) {
+			// log
+			return [];
+		}
+	}
+}
---
+++ test/BafServiceTest.php
@@ -0,0 +1,42 @@
+<?php
+
+use OCA\Files_Confidential\ContentProviders\MicrosoftContentProvider;
+use OCA\Files_Confidential\ContentProviders\OpenDocumentContentProvider;
+use OCP\Files\Folder;
+use OCP\Files\IRootFolder;
+use Test\TestCase;
+
+/**
+ * @group DB
+ */
+class BafServiceTest extends TestCase {
+	/**
+	 * @return void
+	 * @throws \Psr\Container\ContainerExceptionInterface
+	 * @throws \Psr\Container\NotFoundExceptionInterface
+	 */
+	public function testParser() : void {
+		$xml = file_get_contents(__DIR__ . '/res/example.xml');
+		/** @var \OCA\Files_Confidential\Service\BafService $service */
+		$service = \OC::$server->get(\OCA\Files_Confidential\Service\BafService::class);
+		$labels = $service->parseXml($xml);
+
+		$this->assertCount(4, $labels);
+
+		$this->assertEquals('Non-Business', $labels[0]->getName());
+		$this->assertEquals('Non-Business', $labels[0]->getKeywords()[0]);
+		$this->assertEquals('urn:example:tscp:1:non-business', $labels[0]->getBailsCategories()[0]);
+
+		$this->assertEquals('General Business', $labels[1]->getName());
+		$this->assertEquals('General Business', $labels[1]->getKeywords()[0]);
+		$this->assertEquals('urn:example:tscp:1:general-business', $labels[1]->getBailsCategories()[0]);
+
+		$this->assertEquals('Confidential', $labels[2]->getName());
+		$this->assertEquals('Confidential', $labels[2]->getKeywords()[0]);
+		$this->assertEquals('urn:example:tscp:1:confidential', $labels[2]->getBailsCategories()[0]);
+
+		$this->assertEquals('Internal Only', $labels[3]->getName());
+		$this->assertEquals('Internal Only', $labels[3]->getKeywords()[0]);
+		$this->assertEquals('urn:example:tscp:1:internal-only', $labels[3]->getBailsCategories()[0]);
+	}
+}
