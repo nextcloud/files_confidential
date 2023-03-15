7d17643cf0c4a1726b79948f2340236d1858586a
Marcel Klehr <mklehr@gmx.net>
2023-03-15 12:36:49 +0100

    Check in /vendor
    Signed-off-by: Marcel Klehr <mklehr@gmx.net>

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'cfa4042a7c40487a874b2569e06174fe0366a757',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'cfa4042a7c40487a874b2569e06174fe0366a757',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
