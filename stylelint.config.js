c906b63224c7ac709f4ad19b419edec8f2c4e6c1
Andrey Borysenko <andrey18106x@gmail.com>
2024-03-06 16:58:59 +0200

    fix: add missing files
    Signed-off-by: Andrey Borysenko <andrey18106x@gmail.com>

---
+++ stylelint.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+	extends: 'stylelint-config-recommended-vue',
+	rules: {
+		'selector-type-no-unknown': null,
+		'number-leading-zero': null,
+		'rule-empty-line-before': [
+			'always',
+			{
+				ignore: ['after-comment', 'inside-block'],
+			},
+		],
+		'declaration-empty-line-before': [
+			'never',
+			{
+				ignore: ['after-declaration'],
+			},
+		],
+		'comment-empty-line-before': null,
+		'selector-type-case': null,
+		'selector-list-comma-newline-after': null,
+		'no-descending-specificity': null,
+		'selector-pseudo-element-no-unknown': [
+			true,
+			{
+				ignorePseudoElements: ['v-deep'],
+			},
+		],
+	},
+	plugins: ['stylelint-scss'],
+}
