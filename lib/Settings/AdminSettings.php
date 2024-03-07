95559e1b8e59478d1581be87fe70f711f8b63fc8
Andrey Borysenko <andrey18106x@gmail.com>
2024-03-07 12:30:04 +0200

    use IInitialState instead of deprecated
    Signed-off-by: Andrey Borysenko <andrey18106x@gmail.com>

--- lib/Settings/AdminSettings.php
+++ lib/Settings/AdminSettings.php
@@ -12,13 +12,13 @@ namespace OCA\Files_Confidential\Settings;
 use OCA\Files_Confidential\Service\MatcherService;
 use OCA\Files_Confidential\Service\SettingsService;
 use OCP\AppFramework\Http\TemplateResponse;
-use OCP\IInitialStateService;
+use OCP\AppFramework\Services\IInitialState;
 use OCP\Settings\ISettings;
 
 class AdminSettings implements ISettings {
 	public function __construct(
 		private SettingsService $settingsService,
-		private IInitialStateService $initialState,
+		private IInitialState $initialState,
 		private MatcherService $matcherService
 	) {
 	}
@@ -29,8 +29,8 @@ class AdminSettings implements ISettings {
 	public function getForm(): TemplateResponse {
 		$labels = $this->settingsService->getClassificationLabels();
 		$labels = array_map(fn ($label) => $label->toArray(), $labels);
-		$this->initialState->provideInitialState('files_confidential', 'labels', $labels);
-		$this->initialState->provideInitialState('files_confidential', 'searchExpressions', $this->matcherService->expressions);
+		$this->initialState->provideInitialState('labels', $labels);
+		$this->initialState->provideInitialState('searchExpressions', $this->matcherService->expressions);
 
 		return new TemplateResponse('files_confidential', 'admin');
 	}
