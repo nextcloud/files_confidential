436ddb0167ae5a7c94054cccea805586832d8792
Marcel Klehr <mklehr@gmx.net>
2024-06-03 10:40:00 +0200

    chore: update composer.lock + autoload files
    Signed-off-by: Marcel Klehr <mklehr@gmx.net>

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInit98c95e5ba58866e8940f02b131cb7987
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInit98c95e5ba58866e8940f02b131cb7987', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInit98c95e5ba58866e8940f02b131cb7987', 'loadClassLoader'));
@@ -31,6 +33,25 @@ class ComposerAutoloaderInit98c95e5ba58866e8940f02b131cb7987
 
         $loader->register(true);
 
+        $includeFiles = \Composer\Autoload\ComposerStaticInit98c95e5ba58866e8940f02b131cb7987::$files;
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequire98c95e5ba58866e8940f02b131cb7987($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+/**
+ * @param string $fileIdentifier
+ * @param string $file
+ * @return void
+ */
+function composerRequire98c95e5ba58866e8940f02b131cb7987($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+        require $file;
     }
 }
