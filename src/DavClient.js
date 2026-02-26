1cc5c27ccbfc6e5288b48e0293c9ff0c2ff6a626
Lukas Schaefer <lukas@lschaefer.xyz>
2026-02-26 09:40:11 -0500

    Use nextcloud files instead of webdav library
    Signed-off-by: Lukas Schaefer <lukas@lschaefer.xyz>

--- src/DavClient.js
+++ src/DavClient.js
@@ -3,20 +3,15 @@
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
-import { createClient, getPatcher } from 'webdav'
-import axios from '@nextcloud/axios'
 import parseUrl from 'url-parse'
 import { generateRemoteUrl } from '@nextcloud/router'
+import { getClient } from '@nextcloud/files/dav'
 
 export const rootPath = 'dav'
 
-// force our axios
-const patcher = getPatcher()
-patcher.patch('request', axios)
-
 // init webdav client on default dav endpoint
 const remote = generateRemoteUrl(rootPath)
-const client = createClient(remote)
+const client = getClient(remote)
 
 export const remotePath = parseUrl(remote).pathname
 export default client
